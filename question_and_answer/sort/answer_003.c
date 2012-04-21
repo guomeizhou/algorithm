af1bd79492b91ab4c1dc4fe84d15ee703cbf9525
meizhou guo <gmeizhou@163.com>
2012-04-21 17:52:36 +0800

    第003个问题的答案
    Signed-off-by: meizhou guo <gmeizhou@163.com>

--- question_and_answer/sort/answer_003.c
+++ question_and_answer/sort/answer_003.c
@@ -5,7 +5,8 @@
 #include <time.h>
 
 /*
- * 解法: 在一个范围中随机产生一张牌，取出该张牌，然后缩小范围，如此继续下去。
+ * 解法1: 对应函数Deal，在一个范围中随机产生一张牌，取出该张牌，然后缩小范围，如此继续下去。
+ * 解法2: 对应函数DealEx，先产生54张随机牌，然后分别从2, 3, ......, 51张牌中产生牌发给3个人中的一个。
  */
 
 /* 程序输出如下:
@@ -19,16 +20,38 @@
 
 [********** After Deal **********]
 Person 1:
-黑桃A, 黑桃2, 梅花3, 红桃9, 红桃J, 黑桃Q, 红桃3, 黑桃3, 梅花2, 大王,
-方块8, 黑桃7, 梅花A, 梅花K, 方块5, 梅花4, 红桃7
+方块4, 黑桃3, 红桃5, 红桃J, 方块Q, 红桃7, 红桃K, 方块3, 梅花5, 黑桃9,
+黑桃4, 红桃2, 黑桃5, 黑桃7, 黑桃2, 方块8, 小王
 Person 2:
-小王, 红桃5, 梅花8, 黑桃K, 黑桃8, 方块10, 黑桃4, 方块7, 方块Q, 梅花10,
-红桃10, 梅花Q, 红桃A, 黑桃5, 梅花7, 红桃Q, 红桃6
+红桃8, 梅花4, 梅花J, 红桃3, 黑桃A, 黑桃K, 黑桃J, 梅花K, 梅花Q, 方块9,
+梅花7, 黑桃10, 方块6, 方块A, 方块5, 梅花6, 梅花3
 Person 3:
-黑桃6, 黑桃10, 黑桃9, 方块4, 红桃8, 方块J, 方块2, 方块6, 方块A, 红桃4,
-梅花J, 红桃K, 红桃2, 方块9, 梅花5, 方块K, 梅花6
+大王, 红桃9, 方块2, 红桃4, 红桃Q, 红桃10, 红桃6, 红桃A, 方块7, 梅花8,
+梅花2, 梅花9, 方块K, 梅花10, 方块10, 黑桃6, 梅花A
 Remain:
-梅花9, 黑桃J, 方块3
+黑桃Q, 方块J, 黑桃8
+
+
+[********** Before DealEx **********]
+方块A, 方块2, 方块3, 方块4, 方块5, 方块6, 方块7, 方块8, 方块9, 方块10,
+方块J, 方块Q, 方块K, 梅花A, 梅花2, 梅花3, 梅花4, 梅花5, 梅花6, 梅花7,
+梅花8, 梅花9, 梅花10, 梅花J, 梅花Q, 梅花K, 红桃A, 红桃2, 红桃3, 红桃4,
+红桃5, 红桃6, 红桃7, 红桃8, 红桃9, 红桃10, 红桃J, 红桃Q, 红桃K, 黑桃A,
+黑桃2, 黑桃3, 黑桃4, 黑桃5, 黑桃6, 黑桃7, 黑桃8, 黑桃9, 黑桃10, 黑桃J,
+黑桃Q, 黑桃K, 小王, 大王
+
+[********** After DealEx **********]
+Person 1:
+黑桃8, 梅花3, 梅花6, 梅花A, 方块A, 方块6, 黑桃10, 梅花7, 方块9, 梅花Q,
+梅花K, 黑桃J, 黑桃K, 黑桃A, 红桃3, 梅花J, 梅花4
+Person 2:
+方块J, 小王, 方块8, 黑桃2, 黑桃7, 方块5, 红桃2, 黑桃4, 黑桃9, 梅花5,
+方块3, 红桃K, 红桃7, 方块Q, 红桃J, 红桃5, 黑桃3
+Person 3:
+黑桃Q, 黑桃6, 方块10, 梅花10, 方块K, 黑桃5, 梅花2, 梅花8, 方块7, 红桃A,
+红桃6, 红桃10, 红桃Q, 红桃4, 方块2, 红桃9, 梅花9
+Remain:
+大王, 红桃8, 方块4
 */
 
 #define POKER_NUM 54
@@ -36,6 +59,10 @@ Remain:
 #define EVERY_PERSON_POKER_NUM (POKER_NUM - REMAIN_POKER_NUM) / 3
 #define PART_POKER_NUM 13
 #define POKER_COLOR_NUM 4
+
+typedef int BOOL;
+#define TRUE 1
+#define FALSE 0
 
 typedef enum 
 {
@@ -59,6 +86,13 @@ typedef struct
 	PokerType pt;
 } Poker;
 
+typedef struct
+{
+	Poker p;
+	// 标记该数的索引值是否已经随机产生过
+	BOOL bFlag;
+} PokerEx;
+
 /*
  * 功能: 发牌。
          循环经过(nLength - nRemainLength)趟，每一趟的循环如下：
@@ -78,6 +112,35 @@ typedef struct
  */
 void Deal(const Poker* pPoker, int nLength, Poker* pPoker1, Poker* pPoker2, Poker* pPoker3, int nEveryPersonLength,
 			Poker* pPokerRemain, int nRemainLength);
+void OutputDealBefore(const Poker* pPoker, int nPokerNum);
+void OutputDealAfter(const Poker* pPoker1, const Poker* pPoker2, const Poker* pPoker3, const Poker* pPokerRemain,
+					 int nEveryPersonPokerNum, int nRemainPokerNum);
+
+/*
+ * 功能: 发牌。
+         先随机产生54张牌放入pTempPokerEx中，
+         然后随机产生一个[0, i - 1]范围内的数nPokerExIndex，
+		 如果pTempPokerEx[nPokerExIndex]的标记位为TRUE，就把pTempPokerEx[i - 1]标记为TRUE，
+		                 并把该pTempPokerEx[i - 1]牌发给3个人中的一个；
+		 如果pTempPokerEx[nPokerExIndex]的标记位为FALSE，就把pTempPokerEx[nPokerExIndex]标记为TRUE，
+		                 并把该pTempPokerEx[nPokerExIndex]牌发给3个人中的一个。
+ * 名称: DealEx。
+ * 参数: const Poker* pPoker，指向一个数组，该数组数据是输入数据。
+ * 参数: int nLength，pPoker数组的长度。
+ * 参数: Poker* pPoker1，指向存放第1个人牌的数组，该数组数据是输出数据。
+ * 参数: Poker* pPoker2，指向存放第2个人牌的数组，该数组数据是输出数据。
+ * 参数: Poker* pPoker3，指向存放第3个人牌的数组，该数组数据是输出数据。
+ * 参数: int nEveryPersonLength，pPoker1或pPoker2或pPoker3数组的长度。
+ * 参数: Poker* pPokerRemain，指向存放剩余未发牌的数组，该数组数据是输出数据。
+ * 参数: int nRemainLength，pPokerRemain数组的长度。
+ * 返回值: void。
+ */
+void DealEx(const Poker* pPoker, int nLength, Poker* pPoker1, Poker* pPoker2, Poker* pPoker3, int nEveryPersonLength,
+		  Poker* pPokerRemain, int nRemainLength);
+void OutputDealExBefore(const Poker* pPoker, int nPokerNum);
+void OutputDealExAfter(const Poker* pPoker1, const Poker* pPoker2, const Poker* pPoker3, const Poker* pPokerRemain,
+					 int nEveryPersonPokerNum, int nRemainPokerNum);
+
 void Output(const Poker* pPoker, int nLength);
 
 int main()
@@ -99,19 +162,19 @@ int main()
 	Poker arrPoker3[EVERY_PERSON_POKER_NUM] = {0};
 	Poker arrPokerRemain[REMAIN_POKER_NUM] = {0};
 
-	printf("[********** Before Deal **********]\n");
-	Output(arrPoker, POKER_NUM);
+	OutputDealBefore(arrPoker, POKER_NUM);
 	Deal(arrPoker, POKER_NUM, arrPoker1, arrPoker2, arrPoker3, EVERY_PERSON_POKER_NUM, arrPokerRemain, REMAIN_POKER_NUM);
+	OutputDealAfter(arrPoker1, arrPoker2, arrPoker3, arrPokerRemain, EVERY_PERSON_POKER_NUM, REMAIN_POKER_NUM);
 
-	printf("\n[********** After Deal **********]\n");
-	printf("Person 1:\n");
-	Output(arrPoker1, EVERY_PERSON_POKER_NUM);
-	printf("Person 2:\n");
-	Output(arrPoker2, EVERY_PERSON_POKER_NUM);
-	printf("Person 3:\n");
-	Output(arrPoker3, EVERY_PERSON_POKER_NUM);
-	printf("Remain:\n");
-	Output(arrPokerRemain, REMAIN_POKER_NUM);
+	memset(arrPoker1, 0, sizeof(arrPoker1));
+	memset(arrPoker2, 0, sizeof(arrPoker2));
+	memset(arrPoker3, 0, sizeof(arrPoker3));
+	memset(arrPokerRemain, 0, sizeof(arrPokerRemain));
+
+	printf("\n\n");
+	OutputDealExBefore(arrPoker, POKER_NUM);
+	DealEx(arrPoker, POKER_NUM, arrPoker1, arrPoker2, arrPoker3, EVERY_PERSON_POKER_NUM, arrPokerRemain, REMAIN_POKER_NUM);
+	OutputDealExAfter(arrPoker1, arrPoker2, arrPoker3, arrPokerRemain, EVERY_PERSON_POKER_NUM, REMAIN_POKER_NUM);
 
 	return 0;
 }
@@ -171,6 +234,126 @@ void Deal(const Poker* pPoker, int nLength, Poker* pPoker1, Poker* pPoker2, Poke
 
 	free(pTempPoker);
 	pTempPoker = NULL;
+}
+
+void OutputDealBefore(const Poker* pPoker, int nPokerNum)
+{
+	printf("[********** Before Deal **********]\n");
+	Output(pPoker, nPokerNum);
+}
+
+void OutputDealAfter(const Poker* pPoker1, const Poker* pPoker2, const Poker* pPoker3, const Poker* pPokerRemain,
+					 int nEveryPersonPokerNum, int nRemainPokerNum)
+{
+	printf("\n[********** After Deal **********]\n");
+	printf("Person 1:\n");
+	Output(pPoker1, nEveryPersonPokerNum);
+	printf("Person 2:\n");
+	Output(pPoker2, nEveryPersonPokerNum);
+	printf("Person 3:\n");
+	Output(pPoker3, nEveryPersonPokerNum);
+	printf("Remain:\n");
+	Output(pPokerRemain, nRemainPokerNum);
+}
+
+void DealEx(const Poker* pPoker, int nLength, Poker* pPoker1, Poker* pPoker2, Poker* pPoker3, int nEveryPersonLength,
+		  Poker* pPokerRemain, int nRemainLength)
+{
+	int i = 0;
+	int j = 0;
+
+	int nCount1 = 0;
+	int nCount2 = 0;
+	int nCount3 = 0;
+
+	int nPokerExIndex = 0;
+	PokerEx pokerExTemp = {0};
+	PokerEx* pTempPokerEx = (PokerEx*)malloc(sizeof(PokerEx) * nLength);
+
+	for (i = 0; i < nLength; ++i)
+	{
+		pTempPokerEx[i].p = pPoker[i];
+		pTempPokerEx[i].bFlag = FALSE;
+	}
+	srand((unsigned int)time(NULL));
+	for (i = nLength; i > 1; --i)
+	{
+		nPokerExIndex = rand() % i;
+		if (nPokerExIndex != i - 1)
+		{
+			pokerExTemp = pTempPokerEx[nPokerExIndex];
+			pTempPokerEx[nPokerExIndex] = pTempPokerEx[i - 1];
+			pTempPokerEx[i - 1] = pokerExTemp;
+		}
+	}
+	
+	for (i = 2; i <= nLength - nRemainLength; ++i)
+	{
+		nPokerExIndex = rand() % i;
+		if (pTempPokerEx[nPokerExIndex].bFlag)
+		{
+			// nPokerExIndex肯定不等于i - 1
+			pTempPokerEx[i - 1].bFlag = TRUE;
+			pokerExTemp = pTempPokerEx[i - 1];
+		}
+		else
+		{
+			pTempPokerEx[nPokerExIndex].bFlag = TRUE;
+			pokerExTemp = pTempPokerEx[nPokerExIndex];
+		}
+
+		++j;
+		if (j == 1)
+		{
+			pPoker1[nCount1++] = pokerExTemp.p;
+		}
+		else if (j == 2)
+		{
+			pPoker2[nCount2++] = pokerExTemp.p;
+		}
+		else
+		{
+			pPoker3[nCount3++] = pokerExTemp.p;
+			j = 0;
+		}
+	}
+
+	for (i = 0; i < nLength - nRemainLength; ++i)
+	{
+		if (!pTempPokerEx[i].bFlag)
+		{
+			pPoker3[nCount3] = pTempPokerEx[i].p;
+			break;
+		}
+	}
+
+	for (i = 0; i < nRemainLength; ++i)
+	{
+		pPokerRemain[i] = pTempPokerEx[nLength - nRemainLength + i].p;
+	}
+
+	free(pTempPokerEx);
+	pTempPokerEx = NULL;
+}
+
+void OutputDealExBefore(const Poker* pPoker, int nPokerNum)
+{
+	printf("[********** Before DealEx **********]\n");
+	Output(pPoker, nPokerNum);
+}
+
+void OutputDealExAfter(const Poker* pPoker1, const Poker* pPoker2, const Poker* pPoker3, const Poker* pPokerRemain,
+					 int nEveryPersonPokerNum, int nRemainPokerNum)
+{
+	printf("\n[********** After DealEx **********]\n");
+	printf("Person 1:\n");
+	Output(pPoker1, nEveryPersonPokerNum);
+	printf("Person 2:\n");
+	Output(pPoker2, nEveryPersonPokerNum);
+	printf("Person 3:\n");
+	Output(pPoker3, nEveryPersonPokerNum);
+	printf("Remain:\n");
+	Output(pPokerRemain, nRemainPokerNum);
 }
 
 void Output(const Poker* pPoker, int nLength)
